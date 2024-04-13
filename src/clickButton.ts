99b8bfdec04b59295d47ac8ada8ac0166e6bb2bf
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-04-13 10:32:34 +0200

    feat(): Add TS features

--- src/clickButton.ts
+++ src/clickButton.ts
@@ -1,5 +1,40 @@
 export class ClickButton {
-    public LogToConsole() {
-        console.log("Hello, World!");
+    public isClickEnabled: boolean = false;
+    public amountOfClicks: number = 0;
+    private secondsToClick: number = 10;
+    public secondsRemaining: number = 10;
+    public countdownSecondsRemaining: number = 3;
+    public isCountdownEnabled: boolean = true;
+    private timerInterval: NodeJS.Timeout | null = null;
+    private countdownInterval: NodeJS.Timeout | null = null;
+    private cps: number;
+
+    private startTimer() {
+        this.isClickEnabled = true;
+        this.timerInterval = setInterval(() => {
+            if (this.secondsRemaining > 0) {
+                this.secondsRemaining--;
+            } else {
+                clearInterval(this.timerInterval);
+                this.isClickEnabled = false;
+                this.cps = this.amountOfClicks / this.secondsToClick;
+            }
+        }, 1000);
+    }
+
+    public startCountdownTimer() {
+        this.isCountdownEnabled = false;
+        this.countdownInterval = setInterval(() => {
+            if (this.countdownSecondsRemaining > 0) {
+                this.countdownSecondsRemaining--;
+            } else {
+                clearInterval(this.countdownInterval);
+                this.startTimer();
+            }
+        }, 1000);
+    }
+    
+    public incrementAmountOfClicks() {
+        this.amountOfClicks++;
     }
 }
