3361b9446630d941eb5fdcfba02a2870a129b8e5
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-04-13 08:56:55 +0200

    feat(): Add text and CSS

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
 export class App {
-  public message = 'Hello World!';
+  public welcomeMessage = 'Aurelia Website';
+  public initialExplanationMessage = 'This is a very simple website for testing how fast one can input left clicks.';
+  public buttonExplanationMessage = 'To start measuring CPS, click the button below. The timer will start as soon as you click.'
 }
